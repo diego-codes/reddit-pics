336d87cb2254057b09e6c06f01de0cebad1fbd30
Diego Hernandez <diego.a.hernandez123@gmail.com>
2021-02-28 10:30:24 -0800

    initial setup

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Index() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Home</main>
+    </div>
+  )
+}
